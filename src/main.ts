7a4c09d56a0c0f7b30a778cc52bd90c1719dff83
renanlucaz <renanluquinhas53@gmail.com>
2024-10-24 20:36:43 -0300

    Ativa cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder().setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
